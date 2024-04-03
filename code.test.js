fc5c2fcbc09571fe886bc7933b29fa2630c8b55f
Noah Mulvaney <127539141+noah-mulvaney@users.noreply.github.com>
2024-04-03 15:00:17 -0600

    Expand tests

--- code.test.js
+++ code.test.js
@@ -4,3 +4,12 @@ eval(fs.readFileSync('code.js')+'');
 
 assert(factorial(10) === 3628800);
 assert(e(10) === 2.7182818011463845);
+
+let e_close_enough = e(1000000);
+
+let prev_err = 1;
+for (let i = 1; i < 10; ++i) {
+  let error = e_close_enough - e(i);
+  assert(error < prev_err);
+  prev_err = error;
+}
