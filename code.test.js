f91165a09e9a004556e6aaf1a06edf4f7b794465
Noah Mulvaney <mulvaney.np@gmail.com>
2024-04-04 12:51:28 -0600

    Reduce size of test

--- code.test.js
+++ code.test.js
@@ -11,7 +11,7 @@ let e_close_enough = 2.71828182845904523536;
 
 // Test to make sure that as n increases, e(n) gives a closer approximation of e
 let prev_err = 1;
-for (let i = 1; i < 50; ++i) {
+for (let i = 1; i < 20; ++i) {
   let error = e_close_enough - e(i);
   assert(error < prev_err);
   prev_err = error;
